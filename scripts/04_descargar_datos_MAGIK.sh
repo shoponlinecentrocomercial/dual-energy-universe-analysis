0d902710c6ec5e68a83bc7433e4394ef46839b6e
shoponlinecentrocomercial <276354192+shoponlinecentrocomercial@users.noreply.github.com>
2026-07-22 19:07:01 +0200

    Add script to download MAGIK TXS 0506+056 data
    Script downloads MAGIK TXS 0506+056 data and provides usage instructions.

---
+++ scripts/04_descargar_datos_MAGIK.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Script para descargar los datos de MAGIK TXS 0506+056
+
+# Ejecutar en la carpeta del script analisis_magik_txs.py 
+wget https://door06.pic.es:8452/MAGIC_TXS0506+056_data/MAGIC_TXS0506+056_data_v01_00.txt?action=show
+
+#IMPORTANTE Renombrar a MAGIK.txt para evitar errores de lectura
